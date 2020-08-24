f2a07c04f5cd48d3c379e37d410316b8b2ccd2c0
OvaisFareed <movaissiddiqui16@gmail.com>
2020-08-24 17:07:51 +0500

    Updates

---
+++ precache-manifest.297a52d71c54b459de5012a0c3a5baab.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c809e7380de67d02a81f72e4977a0f0a",
+    "url": "/sample-react-app/index.html"
+  },
+  {
+    "revision": "3c4bcfaeb270bd4fe8e2",
+    "url": "/sample-react-app/static/css/4.89630fee.chunk.css"
+  },
+  {
+    "revision": "b01e1c20ade956f7d128",
+    "url": "/sample-react-app/static/css/5.bc4920be.chunk.css"
+  },
+  {
+    "revision": "e550d54baa658338c500",
+    "url": "/sample-react-app/static/css/main.5442dbe8.chunk.css"
+  },
+  {
+    "revision": "68c7aaf42fd8155644b0",
+    "url": "/sample-react-app/static/js/2.259906c7.chunk.js"
+  },
+  {
+    "revision": "327f37c7156d8c49b64c",
+    "url": "/sample-react-app/static/js/3.4bf215e5.chunk.js"
+  },
+  {
+    "revision": "3c4bcfaeb270bd4fe8e2",
+    "url": "/sample-react-app/static/js/4.20debf32.chunk.js"
+  },
+  {
+    "revision": "b01e1c20ade956f7d128",
+    "url": "/sample-react-app/static/js/5.36416705.chunk.js"
+  },
+  {
+    "revision": "944aeef7fb0ee52992be",
+    "url": "/sample-react-app/static/js/6.174bebea.chunk.js"
+  },
+  {
+    "revision": "e550d54baa658338c500",
+    "url": "/sample-react-app/static/js/main.9b7b9aa7.chunk.js"
+  },
+  {
+    "revision": "71cc1a33035535c88ef8",
+    "url": "/sample-react-app/static/js/runtime-main.8ae900f6.js"
+  },
+  {
+    "revision": "d2ded907979414fd4c97c903f207d305",
+    "url": "/sample-react-app/static/media/avatar.d2ded907.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/sample-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
